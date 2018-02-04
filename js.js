9bcdddd7a983ac734e01845c1cc99eae2919a6a3
Vinícius Hoyer <vhoyer@live.com>
2018-02-04 19:31:31 -0200

    Computed and methods

--- js.js
+++ js.js
@@ -1,11 +1,28 @@
+let vm1= new Vue({
+	el: '#head',
+	data: {
+		myAweTitle: 'This is Awe-some!!',
+	},
+});
+
 let vm = new Vue({
 	el: '#app',
 	data: {
-		msg: 'wtf',
+		msg: 'THE HEAD CAN BE A VUE INSTANCE!',
 	},
 	watch: {
 		msg: function(n,o){
 			console.log('yup, that happens before the return ... impressive');
+		},
+	},
+	methods: {
+		reverse: function(msg){
+			return msg.split('').reverse().join('');
+		},
+	},
+	computed: {
+		reversedMsg: function(){
+			return this.msg.split('').reverse().join('');
 		},
 	},
 });
