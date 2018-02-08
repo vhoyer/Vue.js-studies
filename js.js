c6cbf0922a9dffc2bfdd5497a55933085ecfa0d3
Vinícius Hoyer <vhoyer@live.com>
2018-02-08 18:25:48 -0200

    Swaping and keeping still

--- js.js
+++ js.js
@@ -8,15 +8,19 @@ let vm1= new Vue({
 let vm = new Vue({
 	el: '#app',
 	data: {
-		nowD: Date.now(),
-		nowDK: Date.now(),
+		list: [
+		{ id: 0, display: 'wtf0',},
+		{ id: 1, display: 'wtf1',},
+		{ id: 2, display: 'wtf2',},
+		{ id: 3, display: 'wtf3',},
+		{ id: 4, display: 'wtf4',},
+		],
 	},
-	computed: {
-		nowC: function(){
-			setInterval(()=>{this.nowD=Date.now();},1000);
-			import('./other.js')
-				.then(x => this.nowDK = x.func1());
-			return "whatever"
+	methods: {
+		swap: function(){
+			let x = this.list[0];
+			this.list.shift();
+			this.list.push(x);
 		},
 	},
 });
